c92e77073299245efe26584fab41630e30b90567
Matěj Verhaegen <matej@appelevate.cz>
2025-05-30 08:34:47 +0200

    fix button being enabled at the wrong time

--- lib/src/logic/ordering.dart
+++ lib/src/logic/ordering.dart
@@ -147,14 +147,15 @@ StavJidla getStavJidla(BuildContext context, Jidlo dish) {
 
 bool isButtonEnabled(StavJidla stavJidla) {
   switch (stavJidla) {
-    case StavJidla.dostupneNaBurze:
-    case StavJidla.vlozenoNaBurze:
-    case StavJidla.neobjednano:
+    case StavJidla.nedostupne:
+    case StavJidla.objednanoVyprsenaPlatnost:
+      return false;
     case StavJidla.objednano:
     case StavJidla.objednanoPouzeNaBurzu:
+    case StavJidla.vlozenoNaBurze:
+    case StavJidla.dostupneNaBurze:
+    case StavJidla.neobjednano:
       return true;
-    default:
-      return false;
   }
 }
 
