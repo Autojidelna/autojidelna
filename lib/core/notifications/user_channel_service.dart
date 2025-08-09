1630cf134dd6641f03a17c6350884be32a410565
mattheroit <mattheroit@pm.me>
2025-08-10 01:43:56 +0200

    move channel stuff from NotificationTopics to UserChannelService

--- lib/core/notifications/user_channel_service.dart
+++ lib/core/notifications/user_channel_service.dart
@@ -1,13 +1,18 @@
+import 'package:autojidelna/app/app.dart';
 import 'package:autojidelna/core/notifications/notification_topics.dart';
 import 'package:autojidelna/core/types/freezed/safe_account.dart/safe_account.dart';
 import 'package:autojidelna/core/utils/url.dart';
+import 'package:autojidelna/l10n/output/l10n.dart';
+import 'package:autojidelna/shared/localization/current_locale.dart';
 
 import 'package:awesome_notifications/awesome_notifications.dart';
 
 class UserChannelService {
-  String userIdGen(SafeAccount account) => '${Url.clean(account.url)}-${account.username}';
-  String getChannelKey(String userId, String topic) => '$userId-$topic';
-  String getGroup(String topic) => 'group-$topic';
+  static final L10n _l10n = lookupL10n(App.globalContainer.read(currentLocaleProvider));
+
+  static String userIdGen(SafeAccount account) => '${Url.clean(account.url)}-${account.username}';
+  static String getChannelKey(String userId, String topic) => '$userId-$topic';
+  static String getGroup(String topic) => 'group-$topic';
 
   Future<void> createChannelsForUser(SafeAccount account) async {
     String userId = userIdGen(account);
@@ -19,7 +24,7 @@ class UserChannelService {
         NotificationChannel(
           channelKey: channelKey,
           channelName: account.username,
-          channelDescription: NotificationTopics.channelDescriptions[topic],
+          channelDescription: _channelDescriptions[topic],
           channelGroupKey: channelGroupKey,
           importance: NotificationImportance.High,
         ),
@@ -33,4 +38,16 @@ class UserChannelService {
       await AwesomeNotifications().removeChannel(getChannelKey(userId, topic));
     }
   }
+
+  static final Map<String, String> _channelDescriptions = {
+    'food_today_1100': _l10n.notificationFoodTodayDescription,
+    'low_credit_1500': _l10n.notificationLowCreditDescription,
+    'next_week_food_check_1500': _l10n.notificationNextWeekFoodCheckDescription,
+  };
+
+  static List<NotificationChannelGroup> channelGroups = [
+    NotificationChannelGroup(channelGroupKey: getGroup(NotificationTopics.foodToday), channelGroupName: _l10n.channelNameDish),
+    NotificationChannelGroup(channelGroupKey: getGroup(NotificationTopics.lowCredit), channelGroupName: _l10n.channelNameLowCredit),
+    NotificationChannelGroup(channelGroupKey: getGroup(NotificationTopics.nextWeekFoodCheck), channelGroupName: _l10n.channelNameOrdered),
+  ];
 }
