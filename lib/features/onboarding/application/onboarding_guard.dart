84ed49c06928b83379d08ab49a76a9e12a9ed233
mattheroit <mattheroit@pm.me>
2025-08-10 19:43:06 +0200

    update  onboarding guard

--- lib/features/onboarding/application/onboarding_guard.dart
+++ lib/features/onboarding/application/onboarding_guard.dart
@@ -1,8 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:autojidelna/app/routing/app_router.gr.dart';
-import 'package:autojidelna/features/onboarding/application/step_flow_controller.dart';
 import 'package:autojidelna/shared/config/hive.dart';
 import 'package:autojidelna/shared/providers/account.provider.dart';
+import 'package:autojidelna/features/onboarding/application/step_flow_controller.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 
@@ -14,28 +15,29 @@ class OnboardingGuard extends AutoRouteGuard {
   void onNavigation(NavigationResolver resolver, StackRouter router) async {
     bool isFirstTime = Hive.box(Boxes.appState).get(HiveKeys.appState.firstTime, defaultValue: true);
 
-    if (isFirstTime) {
-      final stepFlow = StepFlowController.instance..reset();
-
-      await ref.read(userProvider).updateLoggedSafeAccounts();
-      int loggedInAccounts = ref.read(userProvider).loggedInAccounts.length;
-
-      if (loggedInAccounts > 1) {
-        stepFlow.addSteps(stepFlow.accountPickerFlowPages);
-      } else if (1 > loggedInAccounts) {
-        stepFlow.addSteps(stepFlow.loginFlowPage);
-      }
-
-      resolver.redirect(
-        OnboardingRoute(
-          onCompletedCallback: (onSuccess) async {
-            Hive.box(Boxes.appState).put(HiveKeys.appState.firstTime, !onSuccess);
-            resolver.next(onSuccess);
-          },
-        ),
-      );
-    } else {
+    if (!isFirstTime) {
       resolver.next(true); // Allow navigation
+      return;
     }
+
+    final stepFlow = StepFlowController.instance..reset();
+
+    await ref.read(userProvider).updateLoggedSafeAccounts();
+    int loggedInAccounts = ref.read(userProvider).loggedInAccounts.length;
+
+    if (loggedInAccounts > 1) {
+      stepFlow.addSteps(stepFlow.accountPickerFlowPages);
+    } else if (1 > loggedInAccounts) {
+      stepFlow.addSteps(stepFlow.loginFlowPage);
+    }
+
+    resolver.redirect(
+      OnboardingRoute(
+        onCompletedCallback: (onSuccess) async {
+          Hive.box(Boxes.appState).put(HiveKeys.appState.firstTime, !onSuccess);
+          resolver.next(onSuccess);
+        },
+      ),
+    );
   }
 }
