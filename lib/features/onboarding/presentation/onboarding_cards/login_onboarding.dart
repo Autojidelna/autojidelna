34e01d241b0160394bf314edd06b6424e72053c4
mattheroit <mattheroit@pm.me>
2025-09-02 14:20:30 +0200

    move login logic out of login provider into login_onboarding

--- lib/features/onboarding/presentation/onboarding_cards/login_onboarding.dart
+++ lib/features/onboarding/presentation/onboarding_cards/login_onboarding.dart
@@ -1,10 +1,17 @@
+import 'package:autojidelna/l10n/l10n_context_extension.dart';
 import 'package:autojidelna/core/remote-config/remote_config.dart';
+import 'package:autojidelna/core/types/errors.dart';
+import 'package:autojidelna/core/types/freezed/account/account.dart';
 import 'package:autojidelna/core/utils/url.dart';
+import 'package:autojidelna/shared/config/errors.dart';
+import 'package:autojidelna/shared/providers/account.provider.dart';
+import 'package:autojidelna/shared/providers/disable_interactions_provider.dart';
+import 'package:autojidelna/shared/snackbars/show_internet_connection_snack_bar.dart';
+import 'package:autojidelna/shared/utils/show_snack_bar.dart';
+import 'package:autojidelna/shared/widgets/custom_divider.dart';
 import 'package:autojidelna/features/onboarding/application/onboarding_providers.dart';
 import 'package:autojidelna/features/onboarding/domain/onboarding_step.dart';
-import 'package:autojidelna/features/auth/data/login.provider.dart';
-import 'package:autojidelna/l10n/l10n_context_extension.dart';
-import 'package:autojidelna/shared/widgets/custom_divider.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -74,7 +81,47 @@ class LoginOnboarding extends ConsumerWidget implements OnboardingStep {
   Future<bool> onPreviousPage(BuildContext context, WidgetRef ref) async => true;
 
   @override
-  Future<bool> onNextPage(BuildContext context, WidgetRef ref) async => await ref.read(loginProvider).login(context);
+  Future<bool> onNextPage(BuildContext context, WidgetRef ref) async {
+    final formKey = ref.read(formKeyProvider(FormKeys.credentials));
+    final disableInteractionsNotifier = ref.read(disableInteractions.notifier);
+
+    if (!formKey.currentState!.validate()) return false;
+    formKey.currentState!.save();
+
+    for (var field in OnboardingFields.values) {
+      ref.read(textFieldProvider(field).notifier).setError(null);
+    }
+
+    disableInteractionsNotifier.state = true;
+    bool allowNextPage = false;
+
+    final account = Account(
+      username: ref.read(textFieldProvider(OnboardingFields.username)).value!,
+      password: ref.read(textFieldProvider(OnboardingFields.password)).value!,
+      url: ref.read(textFieldProvider(OnboardingFields.url)).value!,
+    );
+
+    try {
+      await ref.read(userProvider).login(account);
+      allowNextPage = true;
+    } catch (e) {
+      switch (e) {
+        case AuthErrors.noInternetConnection:
+          if (await showInternetConnectionSnackBar() && context.mounted) onNextPage(context, ref);
+          break;
+        case AuthErrors.wrongCredentials:
+          if (context.mounted) ref.read(textFieldProvider(OnboardingFields.password).notifier).setError(context.l10n.errorsWrongCredentialsTextField);
+          break;
+        case AuthErrors.wrongUrl:
+          break;
+        default:
+          showErrorSnackBar(SnackBarAuthErrors.connectionFailed(context.l10n));
+      }
+    }
+
+    ref.read(disableInteractions.notifier).state = false;
+    return allowNextPage;
+  }
 
   @override
   String buttonText(BuildContext context) => context.l10n.login;
