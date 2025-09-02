a4a9283b89f990d4af7895dcb5714e30b381988e
mattheroit <mattheroit@pm.me>
2025-09-02 14:36:10 +0200

    reafactor onNextPage methods

--- lib/features/onboarding/presentation/onboarding_cards/canteen_url_onboarding.dart
+++ lib/features/onboarding/presentation/onboarding_cards/canteen_url_onboarding.dart
@@ -1,10 +1,8 @@
-import 'package:autojidelna/features/onboarding/application/onboarding_providers.dart';
 import 'package:autojidelna/l10n/l10n_context_extension.dart';
 import 'package:autojidelna/core/analytics/analytics_service.dart';
 import 'package:autojidelna/core/types/freezed/account/account.dart';
 import 'package:autojidelna/core/types/errors.dart';
 import 'package:autojidelna/shared/config/errors.dart';
-import 'package:autojidelna/shared/config/hive.dart';
 import 'package:autojidelna/shared/providers/account.provider.dart';
 import 'package:autojidelna/shared/providers/current_canteen.dart';
 import 'package:autojidelna/shared/providers/disable_interactions_provider.dart';
@@ -12,12 +10,12 @@ import 'package:autojidelna/shared/utils/show_snack_bar.dart';
 import 'package:autojidelna/shared/widgets/divider_with_text.dart';
 import 'package:autojidelna/shared/snackbars/show_internet_connection_snack_bar.dart';
 import 'package:autojidelna/features/onboarding/domain/onboarding_step.dart';
+import 'package:autojidelna/features/onboarding/application/onboarding_providers.dart';
 import 'package:autojidelna/features/onboarding/presentation/widgets/canteen_url_picker.dart';
 import 'package:autojidelna/features/onboarding/presentation/widgets/custom_url_field.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive/hive.dart';
 
 class CanteenUrlOnboarding extends StatelessWidget implements OnboardingStep {
   const CanteenUrlOnboarding({super.key});
@@ -40,7 +38,12 @@ class CanteenUrlOnboarding extends StatelessWidget implements OnboardingStep {
   }
 
   @override
-  Future<bool> onPreviousPage(BuildContext context, WidgetRef ref) async => true;
+  Future<bool> onPreviousPage(BuildContext context, WidgetRef ref) async {
+    ref.invalidate(formKeyProvider(FormKeys.url));
+    ref.invalidate(textFieldControllerProvider(OnboardingFields.url));
+    ref.invalidate(textFieldProvider(OnboardingFields.url));
+    return true;
+  }
 
   @override
   Future<bool> onNextPage(BuildContext context, WidgetRef ref) async {
@@ -55,35 +58,36 @@ class CanteenUrlOnboarding extends StatelessWidget implements OnboardingStep {
     }
 
     disableInteractionsNotifier.state = true;
-    bool allowNextPage = true;
+    bool allowNextPage = false;
+
+    final Account account = Account(
+      username: ref.read(textFieldProvider(OnboardingFields.username)).value!,
+      password: ref.read(textFieldProvider(OnboardingFields.password)).value!,
+      url: ref.read(textFieldProvider(OnboardingFields.url)).value!,
+    );
+
     try {
-      await ref.read(userProvider).login(Account(username: '', password: '', url: ref.read(textFieldProvider(OnboardingFields.url)).value ?? ''));
+      await ref.read(userProvider).login(account);
+      allowNextPage = true;
     } catch (e) {
       switch (e) {
-        case AuthErrors.wrongUrl:
-          if (context.mounted) {
-            ref.read(textFieldProvider(OnboardingFields.url).notifier).setError(context.l10n.errorsWrongUrl);
-            allowNextPage = false;
-          }
-          break;
         case AuthErrors.noInternetConnection:
           if (await showInternetConnectionSnackBar() && context.mounted) onNextPage(context, ref);
           break;
         case AuthErrors.connectionFailed:
           if (context.mounted) showErrorSnackBar(SnackBarAuthErrors.connectionFailed(context.l10n));
-          allowNextPage = false;
+          break;
+        case AuthErrors.wrongUrl:
+          if (context.mounted) {
+            ref.read(textFieldProvider(OnboardingFields.url).notifier).setError(context.l10n.errorsWrongUrl);
+          }
           break;
         default:
       }
     }
-    Hive.box(Boxes.appState).put(HiveKeys.appState.url, ref.read(textFieldProvider(OnboardingFields.url)).value);
-
-    disableInteractionsNotifier.state = false;
-    ref.read(textFieldControllerProvider(OnboardingFields.username)).clear();
-    ref.read(textFieldControllerProvider(OnboardingFields.password)).clear();
 
     AnalyticsService.instance.logCanteenUrl(ref.read(textFieldProvider(OnboardingFields.url)).value!, ref.read(currentCanteen).verze);
-
+    disableInteractionsNotifier.state = false;
     return allowNextPage;
   }
 
