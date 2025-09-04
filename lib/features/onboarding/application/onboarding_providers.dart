d5865c2e99339236916fb34b935a4870f7499cc2
mattheroit <mattheroit@pm.me>
2025-09-04 15:55:39 +0200

    update providers again

--- lib/features/onboarding/application/onboarding_providers.dart
+++ lib/features/onboarding/application/onboarding_providers.dart
@@ -1,42 +1,38 @@
-import 'package:autojidelna/shared/config/hive.dart';
 import 'package:autojidelna/shared/providers/text_fields/text_field_state.dart';
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive/hive.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'onboarding_providers.g.dart';
 
 enum OnboardingFormKeys { url, credentials }
 
 enum OnboardingTextFields { url, username, password }
 
-final onboardingFormKeyProvider = Provider.family<GlobalKey<FormState>, OnboardingFormKeys>((ref, formName) => GlobalKey<FormState>());
+@Riverpod(keepAlive: true, dependencies: [])
+GlobalKey<FormState> onboardingFormKey(Ref ref, OnboardingFormKeys formKey) => GlobalKey<FormState>();
 
-final onboardingTextFieldControllerProvider = Provider.family<TextEditingController, OnboardingTextFields>((ref, fieldKey) {
-  String? initialValue = fieldKey == OnboardingTextFields.url ? Hive.box(Boxes.appState).get(HiveKeys.appState.url) : null;
-  final controller = TextEditingController(text: initialValue);
-  ref.onDispose(controller.dispose);
+@Riverpod(dependencies: [])
+class OnboardingFocusNodeFocus extends _$OnboardingFocusNodeFocus {
+  @override
+  bool build(OnboardingTextFields fieldKey) => false;
 
-  return controller;
-});
+  @override
+  set state(bool newState) => super.state = newState;
+  bool update(bool Function(bool state) cb) => state = cb(state);
+}
 
-// TODO: think of a nice way to update this to 2.0.0+ annotation
-final onboardingFocusNodeProvider = ChangeNotifierProvider.family<FocusNode, OnboardingTextFields>((ref, field) {
-  final node = FocusNode();
-  ref.onDispose(node.dispose);
-  return node;
-});
+@Riverpod(dependencies: [OnboardingFocusNodeFocus])
+bool onboardingHasFocus(Ref ref) => OnboardingTextFields.values.any((field) {
+      final hasFocus = OnboardingTextFields.values.any((field) => ref.watch(onboardingFocusNodeFocusProvider(field)));
+      return hasFocus;
+    });
 
-final onboardingHasFocusProvider = Provider<bool>((Ref ref) {
-  return OnboardingTextFields.values.any((field) => ref.watch(onboardingFocusNodeProvider(field)).hasFocus);
-});
-
-// TODO: find way to update this to 2.0.0+ annotation
-final onboardingTextFieldStateProvider =
-    StateNotifierProvider.family<TextFieldNotifier, TextFieldState, OnboardingTextFields>((ref, fieldKey) => TextFieldNotifier());
-
-// TODO: find way to update this to 2.0.0+ annotation
-class TextFieldNotifier extends StateNotifier<TextFieldState> {
-  TextFieldNotifier({String? initialValue}) : super(TextFieldState(value: initialValue ?? ''));
+@Riverpod(keepAlive: true, dependencies: [])
+class OnboardingTextFieldState extends _$OnboardingTextFieldState {
+  @override
+  TextFieldState build(OnboardingTextFields fieldKey) => const TextFieldState(value: '');
 
   void setValue(String? value) => state = state.copyWith(value: value);
   void setError(String? error) => state = state.copyWith(error: error);
