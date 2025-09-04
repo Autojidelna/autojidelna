854d614395c1898bc232dd19f5a9199415473318
mattheroit <mattheroit@pm.me>
2025-09-04 04:15:12 +0200

    convert generated providers back to classic ones

--- lib/features/onboarding/application/onboarding_providers.dart
+++ lib/features/onboarding/application/onboarding_providers.dart
@@ -3,25 +3,21 @@ import 'package:autojidelna/shared/providers/text_fields/text_field_state.dart';
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:hive/hive.dart';
-
-part 'onboarding_providers.g.dart';
 
 enum OnboardingFormKeys { url, credentials }
 
 enum OnboardingTextFields { url, username, password }
 
-@Riverpod(keepAlive: true)
-GlobalKey<FormState> onboardingFormKey(Ref ref, OnboardingFormKeys formKey) => GlobalKey<FormState>();
+final onboardingFormKeyProvider = Provider.family<GlobalKey<FormState>, OnboardingFormKeys>((ref, formName) => GlobalKey<FormState>());
 
-@Riverpod(keepAlive: true)
-Raw<TextEditingController> onboardingTextFieldController(Ref ref, OnboardingTextFields fieldKey) {
+final onboardingTextFieldControllerProvider = Provider.family<TextEditingController, OnboardingTextFields>((ref, fieldKey) {
   String? initialValue = fieldKey == OnboardingTextFields.url ? Hive.box(Boxes.appState).get(HiveKeys.appState.url) : null;
   final controller = TextEditingController(text: initialValue);
   ref.onDispose(controller.dispose);
+
   return controller;
-}
+});
 
 // TODO: think of a nice way to update this to 2.0.0+ annotation
 final onboardingFocusNodeProvider = ChangeNotifierProvider.family<FocusNode, OnboardingTextFields>((ref, field) {
@@ -34,10 +30,13 @@ final onboardingHasFocusProvider = Provider<bool>((Ref ref) {
   return OnboardingTextFields.values.any((field) => ref.watch(onboardingFocusNodeProvider(field)).hasFocus);
 });
 
-@Riverpod(keepAlive: true)
-class OnboardingTextFieldState extends _$OnboardingTextFieldState {
-  @override
-  TextFieldState build(OnboardingTextFields fieldKey) => const TextFieldState(value: '');
+// TODO: find way to update this to 2.0.0+ annotation
+final onboardingTextFieldStateProvider =
+    StateNotifierProvider.family<TextFieldNotifier, TextFieldState, OnboardingTextFields>((ref, fieldKey) => TextFieldNotifier());
+
+// TODO: find way to update this to 2.0.0+ annotation
+class TextFieldNotifier extends StateNotifier<TextFieldState> {
+  TextFieldNotifier({String? initialValue}) : super(TextFieldState(value: initialValue ?? ''));
 
   void setValue(String? value) => state = state.copyWith(value: value);
   void setError(String? error) => state = state.copyWith(error: error);
