0b3b9433afa3e5e1b4b7e3d1fcb5fdac81a0995b
mattheroit <mattheroit@pm.me>
2025-09-15 12:41:08 +0200

    add error method to CrashlyticsService

--- lib/core/crashlytics/crashlytics_service.dart
+++ lib/core/crashlytics/crashlytics_service.dart
@@ -1,10 +1,12 @@
-import 'dart:async';
-
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 class CrashlyticsService {
-  static void enabled(bool enabled) {
-    unawaited(FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(enabled));
-    if (!enabled) unawaited(FirebaseCrashlytics.instance.deleteUnsentReports());
+  static void enabled(bool enabled) async {
+    FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(enabled);
+    if (!enabled) FirebaseCrashlytics.instance.deleteUnsentReports();
+  }
+
+  static void error(dynamic e, StackTrace st) async {
+    FirebaseCrashlytics.instance.recordError(e, st);
   }
 }
