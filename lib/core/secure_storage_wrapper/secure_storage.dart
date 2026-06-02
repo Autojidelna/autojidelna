ffd2d0632f490534c068400cbc3df90e129a0426
mattheroit <mattheroit@pm.me>
2026-06-02 19:53:39 +0200

    move and refactor secure_storage.dart

---
+++ lib/core/secure_storage_wrapper/secure_storage.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+
+import 'package:autojidelna/core/types/errors.dart';
+import 'package:autojidelna/core/types/freezed/account.dart';
+import 'package:autojidelna/core/types/freezed/safe_account.dart';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+/// Secure storage keys for FlutterSecureStorage
+class _SecureStorageKeys {
+  static const String lastLogIn = 'last_log_in';
+  static const String safeAccountList = 'safe_account_list';
+  static String accountId(SafeAccount a) => 'psw_${a.username}_${a.url}';
+}
+
+class SecureStorage {
+  static const AndroidOptions _androidOptions = AndroidOptions.biometric(enforceBiometrics: false);
+  static const IOSOptions _iosOptions = IOSOptions(accessibility: KeychainAccessibility.first_unlock);
+
+  static const FlutterSecureStorage instance = FlutterSecureStorage(aOptions: _androidOptions, iOptions: _iosOptions);
+
+  static Future<String?> _read(String key) async {
+    return await instance.read(key: key);
+  }
+
+  static Future<void> _save(String key, String data) async {
+    await instance.write(key: key.toString(), value: data);
+  }
+
+  static Future<void> saveLastLogIn(SafeAccount safeAccount) async {
+    await _save(_SecureStorageKeys.lastLogIn, jsonEncode(safeAccount));
+  }
+
+  static Future<SafeAccount> readLastLogIn() async {
+    String? val = await _read(_SecureStorageKeys.lastLogIn);
+    if (val == null) throw AuthErrors.accountNotSelected;
+    return SafeAccount.fromJson(jsonDecode(val));
+  }
+
+  static Future<void> saveAccountList(List<SafeAccount> safeAccounts) async {
+    await _save(_SecureStorageKeys.safeAccountList, jsonEncode(safeAccounts));
+  }
+
+  static Future<List<SafeAccount>> readAccountList() async {
+    String? val = await _read(_SecureStorageKeys.safeAccountList);
+    if (val == null) throw AuthErrors.missingCredentials;
+    return jsonDecode(val);
+  }
+
+  static Future<void> saveAccountPassword(Account account) async {
+    await _save(_SecureStorageKeys.accountId(SafeAccount.fromAccount(account)), jsonEncode(account.password));
+  }
+
+  static Future<Account> readAccountPassword(SafeAccount account) async {
+    String? val = await _read(_SecureStorageKeys.accountId(account));
+    if (val == null) throw AuthErrors.accountNotFound;
+    return Account.fromSafeAccount(account, jsonDecode(val));
+  }
+}
